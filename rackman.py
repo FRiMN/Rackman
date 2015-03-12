af7ff927734d6f757e43858a78c47adee818c0c1
Nik Volkov <freezemandix@ya.ru>
2015-03-12 20:19:24 +0700

    1

--- rackman.py
+++ rackman.py
@@ -4,6 +4,8 @@ import pygtk
 pygtk.require('2.0')
 import gtk
 
+
+
 class Master:
     def __init__(self):
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
@@ -45,23 +47,56 @@ class Slave:
         self.window.set_opacity(0.5)
         #self.window.set_size(400, 300)
 
-        #self.window.connect("destroy", lambda w: gtk.main_quit())
-
         self.area = gtk.DrawingArea()
-        self.area.set_size_request(200, 200)
+        #self.area.set_size_request(200, 200)
 
         self.window.add(self.area)
-
-        #self.area.set_events(gtk.gdk.POINTER_MOTION_MASK |
-                            #gtk.gdk.POINTER_MOTION_HINT_MASK )
-        self.area.connect("expose-event", self.draw)
 
         self.area.show()
         self.window.show()
 
+
+        self.window.add_events(gtk.gdk.KEY_PRESS_MASK)
+
+        self.window.connect("check-resize", self.resize_window)
+        self.window.connect("key-press-event", self.resizing)
+        self.area.connect("expose-event", self.draw)
+
+
+
+
         #self.window.begin_resize_drag(gtk.gdk.WINDOW_EDGE_EAST, 1, 10, 10, 1)
 
-    def draw(self, area, event):
+
+    def resizing(self, widget, event):
+        #print event
+
+        old_x, old_y = widget.get_size()
+        new_x, new_y = old_x, old_y
+
+        if event.hardware_keycode == 114:   # right
+            new_x = old_x + 1
+        elif event.hardware_keycode == 116: # down
+            new_y = old_y + 1
+        elif event.hardware_keycode == 113: # left
+            new_x = old_x - 2
+        elif event.hardware_keycode == 111: # up
+            new_y = old_y - 2
+
+        #self.area.set_size_request(new_x, new_y)
+        self.area.window.resize(new_x, new_y)
+        self.window.resize(new_x, new_y)
+
+        return True
+
+
+    def resize_window(self, widget):
+        #print self.window.get_size()
+        self.draw()
+
+
+    def draw(self, area=None, event=None):
+        # http://www.eurion.net/python-snippets/snippet/Drawing%20With%20Cairo.html
         #self.style = self.area.get_style()
         #self.gc = self.style.fg_gc[gtk.STATE_NORMAL]
 
@@ -70,144 +105,34 @@ class Slave:
         self.gc_border = canvas.new_gc(
                     foreground=gtk.gdk.Color(red=0, green=65000, blue=65000),
                     #background=gtk.gdk.Color(green=65535),
-
-                    #font=None,
-                    #function=-1,
-                    fill=1,
-                    #tile=None,
-                    #stipple=None,
-                    #clip_mask=None,
-                    #subwindow_mode=-1,
-#
-                    #ts_x_origin=-1,
-                    #ts_y_origin=-1,
-#
-                    #clip_x_origin=-1,
-                    #clip_y_origin=-1,
-#
-                    #graphics_exposures=-1,
-#
                     line_width=3,
-                    line_style=2,
-#
-                    #cap_style=-1,
-                    #join_style=-1,
+                    line_style=gtk.gdk.LINE_SOLID,
+                    join_style=gtk.gdk.JOIN_MITER,
         )
         self.gc_diagonal = canvas.new_gc(
                     foreground=gtk.gdk.Color(red=0, green=65000, blue=65000),
                     #background=gtk.gdk.Color(green=65535),
-
-                    #font=None,
-                    #function=-1,
-                    fill=1,
-                    #tile=None,
-                    #stipple=None,
-                    #clip_mask=None,
-                    #subwindow_mode=-1,
-#
-                    #ts_x_origin=-1,
-                    #ts_y_origin=-1,
-#
-                    #clip_x_origin=-1,
-                    #clip_y_origin=-1,
-#
-                    #graphics_exposures=-1,
-#
                     line_width=1,
-                    line_style=2,
-#
-                    #cap_style=-1,
-                    #join_style=-1,
+                    line_style=gtk.gdk.LINE_SOLID,
+                    join_style=gtk.gdk.JOIN_MITER,
         )
         self.gc_center = canvas.new_gc(
                     foreground=gtk.gdk.Color(red=0, green=65000, blue=65000),
                     #background=gtk.gdk.Color(green=65535),
-
-                    #font=None,
-                    #function=-1,
-                    fill=1,
-                    #tile=None,
-                    #stipple=None,
-                    #clip_mask=None,
-                    #subwindow_mode=-1,
-
-                    #ts_x_origin=-1,
-                    #ts_y_origin=-1,
-
-                    #clip_x_origin=-1,
-                    #clip_y_origin=-1,
-
-                    #graphics_exposures=-1,
-
                     line_width=1,
-                    line_style=1,
-
-                    #cap_style=-1,
-                    #join_style=-1,
+                    line_style=gtk.gdk.LINE_ON_OFF_DASH,
+                    join_style=gtk.gdk.JOIN_MITER,
         )
 
-        #canvas.draw_line(self.gc, 10, 10, 20, 30)
-        canvas.draw_rectangle(self.gc_border, False, 0,0, 199,199)
+        x, y = self.window.get_size()
 
-        canvas.draw_line(self.gc_diagonal, 0,0, 199,199)
-        canvas.draw_line(self.gc_diagonal, 0,199, 199,0)
+        #canvas.draw_line(self.gc, 10, 10, 20, 30)
+        canvas.draw_rectangle(self.gc_border, False, 0,0, x-1,y-1)
+
+        canvas.draw_line(self.gc_diagonal, 0,0, x-1,y-1)
+        canvas.draw_line(self.gc_diagonal, 0,y-1, x-1,0)
 
         return True
-
-
-
-class Ruler:
-    xpm_data = [
-    "16 16 3 1",
-    "       c None",
-    ".      c #000000000000",
-    "X      c #FFFFFFFFFFFF",
-    "................",
-    "..            ..",
-    ". .          . .",
-    ".  .        .  .",
-    ".   .      .   .",
-    ".    .    .    .",
-    ".     .  .     .",
-    ".      ..      .",
-    ".      ..      .",
-    ".     .  .     .",
-    ".    .    .    .",
-    ".   .      .   .",
-    ".  .        .  .",
-    ". .          . .",
-    "..            ..",
-    "................"
-    ]
-
-    def __init__(self):
-        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        self.window.set_opacity(0.5)
-        self.window.set_decorated(False)
-
-        self.window.connect("check-resize", self.resize_window)
-
-        #hruler = gtk.HRuler()
-        #vruler = gtk.VRuler()
-#
-        #hruler.set_metric(gtk.PIXELS)
-        #vruler.set_metric(gtk.PIXELS)
-
-        self.window.show()
-
-        pixmap, mask = gtk.gdk.pixmap_create_from_xpm_d(self.window.window,
-                                                        None,
-                                                        self.xpm_data)
-
-        image = gtk.Image()
-        image.set_from_pixmap(pixmap, mask)
-        image.show()
-
-        self.window.add(image)
-
-    def resize_window(self, widget):
-        print self.window.get_size()
-
 
 
 
